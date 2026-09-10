5842d3f3443c77aa98ce806e0dd7230412dd8e0b
yonathankristian10 <yonathankristian10@gmail.com>
2026-09-10 14:26:06 +0700

    tes

---
+++ SmartHome.java
@@ -0,0 +1,86 @@
+interface Connectable {
+    void connect();
+    void disconnect();
+}
+
+interface Switchable {
+    void turnOn();
+    void turnOff();
+}
+
+interface Lockable {
+    void lock();
+    void unlock();
+}
+abstract class SmartDevice {
+    protected String id;
+    protected String nama;
+    protected int daya;
+    protected String status;
+
+    public SmartDevice(String id, String nama, int daya) {
+        this.id = id;
+        this.nama = nama;
+        this.daya = daya;
+        this.status = "Mati";
+    }
+
+    public abstract String getDeviceDetails();
+}
+
+class SmartTv extends SmartDevice implements Connectable, Switchable {
+    private int channel;
+    private int volume;
+    private String koneksi;
+
+    public SmartTv(String id, String nama, int daya, int channel, int volume) {
+        super(id, nama, daya);
+        this.channel = channel;
+        this.volume = volume;
+        this.koneksi = "Tidak Terhubung";
+    }
+
+    public void turnOn() {
+        status = "Menyala";
+    }
+
+    public void turnOff() {
+        status = "Mati";
+    }
+
+    public void connect() {
+        koneksi = "WIFI";
+    }
+
+    public void disconnect() {
+        koneksi = "Tidak Terhubung";
+    }
+
+   
+}
+class SmartSpeaker extends SmartDevice implements Connectable, Switchable {
+    private int volume;
+    private String koneksi;
+
+    public SmartSpeaker(String id, String nama, int daya, int volume) {
+        super(id, nama, daya);
+        this.volume = volume;
+        this.koneksi = "Tidak Terhubung";
+    }
+
+    public void turnOn() {
+        status = "Menyala";
+    }
+
+    public void turnOff() {
+        status = "Mati";
+    }
+
+    public void connect() {
+        koneksi = "BLUETOOTH";
+    }
+
+    public void disconnect() {
+        koneksi = "Tidak Terhubung";
+    }
+}
